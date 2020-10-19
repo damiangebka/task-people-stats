c0cf0607a1b8bfc0a39fda7d53e4e4c01b99eff8
Damian Gebka <d.gebka97@gmail.com>
2020-10-19 20:25:27 +0200

    PeopleStats count zmiana

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
 }
 
@@ -16,6 +19,9 @@ class PeopleStats {
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
+    }
+    public long count() {
+        return people.size();
     }
 }
 
